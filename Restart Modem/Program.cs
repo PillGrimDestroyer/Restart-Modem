56099e54227dce3c7ffe19dd2414ecce88a23871
Roman <megamega4@yandex.ru>
2020-09-25 00:51:28 +0600

    Добавьте файлы проекта.

---
+++ Restart Modem/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace Restart_Modem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort _serialPort;
+            int selectedPosition;
+            string selectedPort;
+
+            string[] ports = SerialPort.GetPortNames();
+
+            Console.WriteLine("Список найденых портов:");
+            Console.WriteLine();
+
+            for (int i = 1; i <= ports.Length; i++)
+            {
+                int index = i - 1;
+                string port = ports[index];
+
+                Console.WriteLine("{0}. {1}", i, port);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Введите номер нужного порта:");
+
+            char key = Console.ReadKey().KeyChar;
+            selectedPosition = Convert.ToInt32(key.ToString()) - 1;
+
+            selectedPort = ports[selectedPosition];
+
+            Console.WriteLine();
+            Console.WriteLine("Начинаю работу");
+
+            _serialPort = new SerialPort(selectedPort, 115200);
+            Tools._pause(1000);
+
+            while (true)
+            {
+                _serialPort.Open();
+                Tools._pause(1000);
+
+                _serialPort.Write("AT+CFUN=0\r\n");
+                Tools._pause(2000);
+
+                _serialPort.Write("AT+CFUN=1\r\n");
+                Tools._pause(1000);
+
+                _serialPort.Close();
+
+                Tools._pause((60 * 1000) * 5);
+            }
+        }
+    }
+}
---
+++ Restart Modem/Tools.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Restart_Modem
+{
+    class Tools
+    {
+        /// <summary>
+        /// Пауза
+        /// </summary>
+        /// <param name="value">Длительность паузы (в миллисекундах)</param>
+        public static void _pause(int value)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            while (sw.ElapsedMilliseconds < value)
+            {
+                Application.DoEvents();
+                Thread.Sleep(5);
+            }
+            sw.Reset();
+        }
+    }
+}
